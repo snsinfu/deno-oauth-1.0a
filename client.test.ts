443420900549e72a9764aa05b43a76e6ef583b34
snsinfu <snsinfu@gmail.com>
2021-03-21 19:57:34 +0900

    Add RFC test case for createBaseString

--- client.test.ts
+++ client.test.ts
@@ -1,10 +1,36 @@
-import { createBaseString, OAuthClient } from "./client.ts";
+import { createBaseParams, createBaseString, OAuthClient } from "./client.ts";
 import { HMAC_SHA1 } from "./sign.ts";
 import { assertEquals } from "./test_deps.ts";
 
+Deno.test("createBaseString - reproduces RFC example", () => {
+  const oauthParams = {
+    oauth_consumer_key: "9djdj82h48djs9d2",
+    oauth_token: "kkk9d7dh3k39sjv7",
+    oauth_signature_method: "HMAC-SHA1",
+    oauth_timestamp: 137131201,
+    oauth_nonce: "7d8f3e4a",
+  };
+  const query = new URLSearchParams("b5=%3D%253D&a3=a&c%40=&a2=r%20b");
+  const body = new URLSearchParams("c2&a3=2+q");
+  const params = createBaseParams(oauthParams, query, body);
+
+  const actual = createBaseString(
+    "POST",
+    "http://example.com/request",
+    params,
+  );
+  const expected =
+    "POST&http%3A%2F%2Fexample.com%2Frequest&a2%3Dr%2520b%26a3%3D2%2520q" +
+    "%26a3%3Da%26b5%3D%253D%25253D%26c%2540%3D%26c2%3D%26oauth_consumer_" +
+    "key%3D9djdj82h48djs9d2%26oauth_nonce%3D7d8f3e4a%26oauth_signature_m" +
+    "ethod%3DHMAC-SHA1%26oauth_timestamp%3D137131201%26oauth_token%3Dkkk" +
+    "9d7dh3k39sjv7";
+
+  assertEquals(actual, expected);
+});
 
 // Taken from ddo/oauth-1.0a.
-Deno.test("createBaseString - returns correct string for a Twitter example", () => {
+Deno.test("createBaseString - reproduces Twitter example", () => {
   const params = [
     {
       key: "oauth_consumer_key",
