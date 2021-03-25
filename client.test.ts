402e21ee2ee001e04615afc7c8c1a2916e341022
snsinfu <snsinfu@gmail.com>
2021-03-26 01:40:45 +0900

    Add test for oauth_body_hash

--- client.test.ts
+++ client.test.ts
@@ -225,6 +225,114 @@ Deno.test("OAuthClient.sign - produces correct parameters (Twitter)", () => {
   );
 });
 
+// Taken from ddo/oauth-1.0a.
+Deno.test("OAuthClient.sign - produces correct SHA1 body hash", () => {
+  const client = new OAuthClient({
+    consumer: {
+      key: "1434affd-4d69-4a1a-bace-cc5c6fe493bc",
+      secret: "932a216f-fb94-43b6-a2d2-e9c6b345cbea",
+    },
+    signature: HMAC_SHA1,
+  });
+
+  const data = {
+    "@context": [
+      "http://purl.imsglobal.org/ctx/lti/v2/ToolProxy",
+    ],
+    "@type": "ToolProxy",
+    "lti_version": "LTI-2p1",
+    "tool_proxy_guid": "0cf04d67-8a0d-4d41-af61-6e8c6fc3e68c",
+    "tool_consumer_profile":
+      "http://canvas.docker/api/lti/accounts/1/tool_consumer_profile/339b6700-e4cb-47c5-a54f-3ee0064921a9",
+    "tool_profile": {
+      "lti_version": "LTI-2p1",
+      "product_instance": {
+        "guid": "fd75124a-140e-470f-944c-114d2d93db40",
+        "product_info": {
+          "product_name": {
+            "default_value": "TestTool",
+            "key": "tool.name",
+          },
+          "product_version": "0.1.0",
+          "product_family": {
+            "code": "testtool",
+            "vendor": {
+              "code": "Example.com",
+              "vendor_name": {
+                "default_value": "Example",
+                "key": "tool.vendor.name",
+              },
+            },
+          },
+        },
+      },
+      "base_url_choice": [
+        {
+          "default_base_url": "http://example.docker/",
+          "selector": {
+            "applies_to": [
+              "MessageHandler",
+            ],
+          },
+        },
+      ],
+      "resource_handler": [
+        {
+          "resource_type": {
+            "code": "testtool",
+          },
+          "resource_name": {
+            "default_value": "TestTool",
+            "key": "testtool.resource.name",
+          },
+          "message": [
+            {
+              "message_type": "basic-lti-launch-request",
+              "path": "lti_launch",
+              "enabled_capability": [
+                "Canvas.placements.courseNavigation",
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    "enabled_capability": [
+      "OAuth.splitSecret",
+    ],
+    "security_contract": {
+      "tp_half_shared_secret":
+        "1c7849d3c9f037a9891575c8508d3aaab6a9e1312b5d0353625f83d68f0d545344f81ff9e1849b6400982a0d3f6bf953c6095265e3b6d700a73f5be94ce5654c",
+    },
+  };
+
+  const params = client.sign(
+    "POST",
+    "http://canvas.docker/api/lti/accounts/1/tool_proxy",
+    {
+      params: {
+        oauth_timestamp: 1484599369,
+        oauth_nonce: "t62lMDp9DLwKZJJbZTpmSAhRINGBEOcF",
+        oauth_version: "1.0",
+      },
+      body: JSON.stringify(data),
+    },
+  );
+
+  assertEquals(
+    params,
+    {
+      oauth_consumer_key: "1434affd-4d69-4a1a-bace-cc5c6fe493bc",
+      oauth_nonce: "t62lMDp9DLwKZJJbZTpmSAhRINGBEOcF",
+      oauth_signature_method: "HMAC-SHA1",
+      oauth_timestamp: 1484599369,
+      oauth_version: "1.0",
+      oauth_body_hash: "xpJzRG6xylVIRRtiigLPKX7iRmM=",
+      oauth_signature: "1Q0U8yhK1bWYguRxlUDs9KHywOE=",
+    },
+  );
+});
+
 // POSTPROCESSING ------------------------------------------------------------
 
 // Taken from ddo/oauth-1.0a.
