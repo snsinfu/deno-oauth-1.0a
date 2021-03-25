df8d01d15d16fc568b1dd56aec8ef656ac53e385
snsinfu <snsinfu@gmail.com>
2021-03-26 01:42:46 +0900

    Style tests in logical order

--- client.test.ts
+++ client.test.ts
@@ -142,47 +142,6 @@ Deno.test("OAuthClient.sign - produces correct HMAC-SHA1 signature (RFC)", () =>
 });
 
 // Taken from ddo/oauth-1.0a.
-Deno.test("OAuthClient.signToHeader - prepends realm", () => {
-  const client = new OAuthClient({
-    consumer: {
-      key: "batch-dbc2cd8c-6ca8-463b-96e2-6d8683eac6fd",
-      secret: "4S4Rvm25CJZWv7HBg5HOhhlRTBSZ7npl",
-    },
-    realm: "https://ddo.me/",
-    signature: HMAC_SHA1,
-  });
-
-  const body = new URLSearchParams();
-  body.append("currentbrowserversion", "1");
-  body.append("currentbrowserversion", "5");
-  body.append("currentbrowserversion", "dfadfadfa");
-
-  const actual = client.signToHeader(
-    "PUT",
-    "http://localhost:3737/rest/profiles/" +
-      "1ea2a42f-e14d-4057-8bcd-3e0b4514a267/properties?alt=json",
-    {
-      params: {
-        oauth_timestamp: 1445951836,
-        oauth_nonce: "tKOQtKan8PHIrIoOlrl17zHkZQ2H5CsP",
-        oauth_version: "1.0",
-      },
-      body: body,
-    },
-  );
-
-  const expected = 'OAuth realm="https://ddo.me/", ' +
-    'oauth_consumer_key="batch-dbc2cd8c-6ca8-463b-96e2-6d8683eac6fd", ' +
-    'oauth_nonce="tKOQtKan8PHIrIoOlrl17zHkZQ2H5CsP", ' +
-    'oauth_signature="ri0lfv4udd2uQmkg5cCPVqLruyk%3D", ' +
-    'oauth_signature_method="HMAC-SHA1", ' +
-    'oauth_timestamp="1445951836", ' +
-    'oauth_version="1.0"';
-
-  assertEquals(actual, expected);
-});
-
-// Taken from ddo/oauth-1.0a.
 Deno.test("OAuthClient.sign - produces correct parameters (Twitter)", () => {
   const client = new OAuthClient({
     consumer: {
@@ -331,6 +290,47 @@ Deno.test("OAuthClient.sign - produces correct SHA1 body hash", () => {
       oauth_signature: "1Q0U8yhK1bWYguRxlUDs9KHywOE=",
     },
   );
+});
+
+// Taken from ddo/oauth-1.0a.
+Deno.test("OAuthClient.signToHeader - prepends realm", () => {
+  const client = new OAuthClient({
+    consumer: {
+      key: "batch-dbc2cd8c-6ca8-463b-96e2-6d8683eac6fd",
+      secret: "4S4Rvm25CJZWv7HBg5HOhhlRTBSZ7npl",
+    },
+    realm: "https://ddo.me/",
+    signature: HMAC_SHA1,
+  });
+
+  const body = new URLSearchParams();
+  body.append("currentbrowserversion", "1");
+  body.append("currentbrowserversion", "5");
+  body.append("currentbrowserversion", "dfadfadfa");
+
+  const actual = client.signToHeader(
+    "PUT",
+    "http://localhost:3737/rest/profiles/" +
+      "1ea2a42f-e14d-4057-8bcd-3e0b4514a267/properties?alt=json",
+    {
+      params: {
+        oauth_timestamp: 1445951836,
+        oauth_nonce: "tKOQtKan8PHIrIoOlrl17zHkZQ2H5CsP",
+        oauth_version: "1.0",
+      },
+      body: body,
+    },
+  );
+
+  const expected = 'OAuth realm="https://ddo.me/", ' +
+    'oauth_consumer_key="batch-dbc2cd8c-6ca8-463b-96e2-6d8683eac6fd", ' +
+    'oauth_nonce="tKOQtKan8PHIrIoOlrl17zHkZQ2H5CsP", ' +
+    'oauth_signature="ri0lfv4udd2uQmkg5cCPVqLruyk%3D", ' +
+    'oauth_signature_method="HMAC-SHA1", ' +
+    'oauth_timestamp="1445951836", ' +
+    'oauth_version="1.0"';
+
+  assertEquals(actual, expected);
 });
 
 // POSTPROCESSING ------------------------------------------------------------
